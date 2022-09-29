fa08844edcecaa99384087da2709f796ef1a4f85
Kevin Yap <keviny@sportsbox.ai>
2022-09-29 11:11:47 -0400

    Added paintings to maze level

--- Assets/Code/Kevin/WallController.cs
+++ Assets/Code/Kevin/WallController.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class WallController : MonoBehaviour
 {
-    public GameObject PosterPrefab;
-
+    public GameObject[] postersPrefabs;
+    public GameObject posterLeft;
+    public GameObject posterRight;
     public int posterChance;
     void Start() {
+        if (posterLeft != null) {
+            Destroy(posterLeft);
+        } else if (posterRight != null) {
+            Destroy(posterRight);
+        }
         RollPoster(-1);
         RollPoster(1);
     }
     void RollPoster(int dir) {
         int roll = Random.Range(0, 100);
-        if (roll < posterChance) {
-            Vector3 pos = transform.forward*0.25f + transform.right * dir * 5;
-            GameObject poster = Instantiate(PosterPrefab, transform.position + pos, transform.rotation*Quaternion.Euler(0, 90, 0));
+        if (roll < posterChance && dir == 1 ? posterRight == null : posterLeft == null) {
+            int posterIndex = Random.Range(0, postersPrefabs.Length);
+            Vector3 pos = transform.forward*0.25f + transform.right * dir * 6;
+            GameObject poster = Instantiate(postersPrefabs[posterIndex], transform.position + pos, transform.rotation*Quaternion.Euler(0, 90, 0));
             poster.transform.parent = gameObject.transform;
+            if (dir == -1) {
+                posterLeft = poster;
+            } else {
+                posterRight = poster;
+            }
         }
     }
 }
