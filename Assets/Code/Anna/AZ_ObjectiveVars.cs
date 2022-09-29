2ea5a77ff4b50295d80392d18b06b0e3313a5d30
William Yang <wy2037@nyu.edu>
2022-09-29 19:27:05 -0400

    Revert "Test"
    This reverts commit 2d542963f606829ff3fb8fef2a0aaf4a0253f85d.

--- Assets/Code/Anna/AZ_ObjectiveVars.cs
+++
@@ -1,7 +0,0 @@
-public static class AZ_ObjectiveVars {
-    public static int collectibles = 0;
-
-    public const int total_collectibles = 5;
-
-    public static bool key_collected = false;
-}
--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     int moveSpeed = 20; // how fast the player moves
     float lookSpeedX = 6; // left/right mouse sensitivity
     float lookSpeedY = 3; // up/down mouse sensitivity
-    int jumpForce = 250; // ammount of force applied to create a jump
+    int jumpForce = 500; // ammount of force applied to create a jump
 
     public Transform camTrans; // a reference to the camera transform
     float xRotation;
@@ -28,9 +28,7 @@ public class PlayerController : MonoBehaviour
         lookSpeedY *= .65f; //.65 is a rough guess based on testing in firefox.
 #endif
         _rigidbody = GetComponent<Rigidbody>(); // Using GetComponent is expensive. Always do it in start and chache it when you can.
-        
-        Cursor.visible = false; 
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked; // Hides the mouse and locks it to the center of the screen.
     }
 
     void FixedUpdate()
@@ -48,9 +46,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (SceneController.instance.paused) {
-            return;
-        }
         yRotation -= Input.GetAxis("Mouse Y") * lookSpeedX;
         xRotation += Input.GetAxis("Mouse X") * lookSpeedY; //inverted
         yRotation = Mathf.Clamp(yRotation, -90, 90); //Keeps up/down head rotation realistic
