07a09c66bd2fe84626e9e9bbd93e14c2e66f7520
William Yang <wy2037@nyu.edu>
2022-09-30 00:58:50 -0400

    Finishing touches pt 3
    Small changes

--- Assets/Code/PlayerCatch.cs
+++ Assets/Code/PlayerCatch.cs
@@ -20,6 +20,7 @@ public class PlayerCatch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Really scuffed method to ensure all guards interact with the catch meter
         current = guard1.playerTimer + guard2.playerTimer + guard3.playerTimer + guard4.playerTimer + guard5.playerTimer + guard6.playerTimer + guard7.playerTimer + guard8.playerTimer + guard9.playerTimer + guard10.playerTimer + guard11.playerTimer + guard12.playerTimer;
         max = guard1.timeToSpot;
         meter.UpdateMeter(max, current);
